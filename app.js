b98e28128571b94a1dae9c5f6a6e3a0dd653e1ac
Darren Cacy <dcacy@us.ibm.com>
2018-06-29 10:36:53 -0500

    cleanup

--- app.js
+++ app.js
@@ -1,50 +1,27 @@
 'use strict';
 require('dotenv').config({silent: true, path: 'local.env'});
 const vcap_services = JSON.parse(process.env.VCAP_SERVICES);
-var debug = require('debug');
+const debug = require('debug');
 const log = debug('dpc-visual-app');
-// log('vcap:', vcap_services.watson_vision_combined[0].credentials.apikey);
-
-var express = require('express');
-
-var cfenv = require('cfenv');
-
-var appEnv = cfenv.getAppEnv();
+// const express = require('express');
+const cfenv = require('cfenv');
+const appEnv = cfenv.getAppEnv();
 process.env.port = appEnv.port;  // for the bot framework
 
-// const SDK = require('./modules/watsonworkspace-sdk/index');
-// var ww;
 
 const handleAction = require('./modules/handleAction');
-// const handleConfig = require('./modules/handleConfig');
 const botFramework = require('watsonworkspace-bot');
-// botFramework.level(process.env.WW_SDK_LOGGER_LEVEL);
-const handleConfig = {};
+botFramework.level(process.env.WW_SDK_LOGGER_LEVEL);
 
 botFramework.startServer();
 log('about to authenticate');
-const customMiddleware = [
-  ['config'],
-  [function(req, res) {handleConfig.config(req,res)}]
-];
-const bot = botFramework.create(
-  // process.env.APP_ID,
-  // process.env.APP_SECRET,
-  // process.env.WEBHOOK_SECRET,
-  // customMiddleware[0],
-  // customMiddleware[1]
-); 
+const bot = botFramework.create(); 
 
-// handleConfig.setBot(bot);
 
-// const bot = botFramework.create(); // bot settings defined by process.env
 bot.authenticate()
 .then( token => {
   log('authentication successful');
-  // ww = new SDK('','', token);
 
-  // serve content from /public (for configuration)
-  botFramework.express.use(express.static(__dirname + '/public'));
   botFramework.express.get('/', (req, res) => {
     res.end('The Visual Recognizer is alive and kicking!');
   });
@@ -54,44 +31,26 @@ bot.authenticate()
   log('error authenticating:', err);
 });
 
-
-// bot.on('message-created', (message) => {
-//   log('found message-created; message is:', message);
-//   // log('actionSelected:', annotation.actionId);
-//   // handleAction.findAsset(message, annotation, bot);
-// });
-
+/*
+Listen for annotations. If it was an image file, then examine the file using Watson Visual Recognition.
+If the annotation was "SHARE", it means the user clicked the share button.
+*/
 bot.on('message-annotation-added', (message, annotation) => {
   if (annotation.context && annotation.context.type === 'file' && annotation.contentType.includes('image')) {
-    log('a file was added:', annotation);
     handleAction.examineFile(message, annotation, bot);
   } else {
-    log('annotation:', annotation);
-    log('and message:', message);
     if (annotation.actionId && annotation.actionId.startsWith('SHARE_')) {
-      log('found SHARE');
       handleAction.shareAnalysis(message, annotation, bot);
     }
   }
 });
 
-// bot.on('actionSelected:View Analysis', (message, annotation, payload) => {
-//   log('View Analysis found:', annotation);
-//   log('payload:', payload);
-//   // log('message is', message);
-//   handleAction.viewAnalysis(message, annotation, payload, bot);
-// });
-
+/*
+Listen for actionSelectd, and if it was View Analysis then show the cards with the classifications
+*/
 bot.on('actionSelected', (message, annotation) => {
   if (annotation.actionId === 'View Analysis') {
     log('actionSelected found:', annotation);
-  handleAction.viewAnalysis(message, annotation, bot);
-}
-  
-  // log('payload:', payload);
-  // log('message is', message);
+    handleAction.viewAnalysis(message, annotation, bot);
+  }
 });
-
-// bot.on('message-focus:ImageHelper', (message, annotation) => {
-//   log('in message-focus:', annotation);
-// });
